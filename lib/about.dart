f7c6153d33bd95bf44b0d34f38ac6583c12d00ab
aninagotgit <aninagotgit@yahoo.com>
2026-08-16 16:23:08 +0200

    add slidable action to about page with placeholder actions

--- lib/about.dart
+++ lib/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:go_router/go_router.dart';
 
 import 'game.dart';
@@ -39,9 +40,9 @@ Widget getAboutPage() {
             'A green tile means you guessed the letter correctly in the correct place.\n'
             'A orange tile means the letter is correct, but in the wrong place.\n'
             'A red tile means the letter is not in the word.\n'
-            'You have 5 guesses.',
+            'You have 5 guesses.\n\n'
+            'Here are the valid bird names you can guess:\n',
           ),
-          Text('Here are the valid bird names you can guess:'),
           Expanded(child: Scroller(list: allLegalGuesses)),
         ],
       ),
@@ -57,16 +58,52 @@ class Scroller extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      padding: const EdgeInsets.all(8),
       itemCount: _list.length,
       itemBuilder: (BuildContext context, int index) {
-        return SizedBox(height: 50, child: Center(child: Text(_list[index])));
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return const SizedBox(
-          height: 1,
-          child: ColoredBox(color: Colors.blueGrey),
+        return Slidable(
+          child: SizedBox(height: 50, child: Center(child: Text(_list[index]))),
+          startActionPane: ActionPane(
+            // A motion is a widget used to control how the pane animates.
+            motion: const ScrollMotion(),
+            // All actions are defined in the children parameter.
+            children: [
+              // A SlidableAction can have an icon and/or a label.
+              SlidableAction(
+                onPressed: (context) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Pressed info ${_list[index]}')),
+                  );
+                },
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                icon: Icons.info,
+                label: 'Info',
+              ),
+            ],
+          ),
+
+          // The end action pane is the one at the right or the bottom side.
+          endActionPane: ActionPane(
+            motion: ScrollMotion(),
+            children: [
+              SlidableAction(
+                onPressed: (context) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Pressed delete ${_list[index]}')),
+                  );
+                },
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Delete',
+              ),
+            ],
+          ),
         );
+        // return SizedBox(height: 30, child: Center(child: Text(_list[index])));
+      },
+      separatorBuilder: (context, index) {
+        return const Divider(color: Colors.grey);
       },
     );
   }
